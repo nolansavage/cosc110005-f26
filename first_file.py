d41a91e07d7a21a4b011151001ea3d00cd7aad3b
nolansavage <nolan.savage@dcmail.ca>
2026-09-14 14:33:09 -0400

    Update first_file.py
    just messing around

--- first_file.py
+++ first_file.py
@@ -1,3 +1,7 @@
 print("hello whats up world")
 
 print("This is the first file I have created for my python journey")
+print("I am excited to learn more about Python!")
+
+name = input("Whats your name?: ")
+print (f"Hello, {name}")
