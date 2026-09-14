7194b3bb5769a482e47b943fcf78dfb3ba07034e
nolansavage <nolan.savage@dcmail.ca>
2026-09-14 14:40:39 -0400

    Delete first_file.py

--- first_file.py
+++
@@ -1,7 +0,0 @@
-print("hello whats up world")
-
-print("This is the first file I have created for my python journey")
-print("I am excited to learn more about Python!")
-
-name = input("Whats your name?: ")
-print (f"Hello, {name}")
