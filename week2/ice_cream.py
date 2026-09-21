3625f789f522614d14368d0bb1183cb48c2f851b
nolansavage <nolan.savage@dcmail.ca>
2026-09-21 13:38:53 -0400

    Update ice_cream.py
    adding comments

--- week2/ice_cream.py
+++ week2/ice_cream.py
@@ -1,10 +1,21 @@
+#getting the number of kiddie cones sold from the user
 kiddie = int(input("Enter number of kiddie cones sold: "))
+
+#getting the number of small cones sold from the user
 small = int(input("Enter number of small cones sold: "))
+
+#getting the number of medium cones sold from the user
 medium = int(input("Enter number of medium cones sold: "))
+
+#getting the number of large cones sold from the user
 large = int(input("Enter number of large cones sold: "))
 
+#calculating the total number of scoops sold
 total_scoops = (kiddie * 0.5) + (small * 1) + (medium * 2) + (large * 3)
 
+#calculating the total amount of ice cream sold in milliliters
 total_ml = total_scoops * 120
 
+
+#displays the total amount of ice cream sold in milliliters
 print("Total ice cream sold:", total_ml, "mL")
